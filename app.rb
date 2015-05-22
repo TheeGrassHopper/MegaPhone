31c6d5b811af6b87af6975ffc94aa3cf3c9e4d3e
alwan mortada <almortada88@gmail.com>
2015-05-22 15:25:04 -0700

    commiting MegaPhoneV1

--- app.rb
+++ app.rb
@@ -1,2 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader'
+
+class MegaPhone
+
+	get '/' do
+		@voice = voice.create(voice: params["voice"], created_at: params[Time.now])
+		erb :index
+	end
+		erb :index
+	
+end
